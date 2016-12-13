ffe035e11de9b0bdccf1f737746af1d9320811ed
David <davidporter33@icloud.com>
2016-12-12 17:13:51 -0800

    completed the remainingtodos

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,5 +1,10 @@
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
+      page('/', articleController.reveal);
+      page('/about', aboutController.reveal);
+
+
+      page();
 
 // TODO: What function do we call to activate page.js?
