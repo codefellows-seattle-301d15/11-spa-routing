d6ed23eb8b34db089e736effe497567575288d9e
tommytnez <tommytnez@yahoo.com>
2016-12-12 16:47:49 -0800

    todone em all

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,5 +1,10 @@
-/* TODO: Configure our two routes for this app with page.js,
+/* TODONE: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
 
-// TODO: What function do we call to activate page.js?
+page('/about', aboutController.reveal);
+page('/', articleController.reveal);
+
+page();
+
+// TODONE: What function do we call to activate page.js?
