ef57f322d52685a45d7351de7e3e2f2e94d99aae
devonhackley <devonhackley@gmail.com>
2016-12-12 16:35:13 -0800

    a couple more todos are done, now we are switching drivers

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,5 +1,5 @@
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
-
+    page('/about', aboutController.reveal);
 // TODO: What function do we call to activate page.js?
